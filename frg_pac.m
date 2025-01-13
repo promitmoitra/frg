d25375940e265dc7ac282e806056a63f6d9ca54b
promitmoitra <promitmoitra@gmail.com>
2025-01-13 23:15:01 +0530

    updated frg_pac to check for processed subid_stress-env_segment, avoid redundant calculation

--- frg_pac.m
+++ frg_pac.m
@@ -9,74 +9,48 @@ dir_sep = '/';
 
 global data_path; data_path = [char(wrk_dir),'data/epoch_data/'];
 
+pac_dir = [data_path '/pac_data/'];
+
 cd(eeglab_dir)
 eeglab nogui;
 cd(data_path)
 
-set_fnames = dir('**/*.set');
+set_fnames = dir('./*.set');
 fname_split = split({set_fnames(:).name},'_');
 subids = unique(str2num(char(fname_split(:,:,1))));
 state_conditions = {'preshort','prelong', 'postshort', 'postlong'};
+trial_categories = {'early','mid','late','leave'};
 
 %%
 for idx = 1:length(subids)
     global subid; subid = subids(idx);
     for sc_idx = 1:length(state_conditions)
-        early = pop_loadset('filename', [num2str(subid) '_' state_conditions{sc_idx} '_early.set'],...
-                'filepath',data_path);
-        try
-            mid = pop_loadset('filename', [num2str(subid)  '_' state_conditions{sc_idx} '_mid.set'],...
-                    'filepath',data_path);
-        catch
-            mid = struct();
-            sprintf('no mid trials for %d',subid)
+        for cat_idx = 1:length(trial_categories)
+            fname = [num2str(subid)  '_' state_conditions{sc_idx},'_' trial_categories{cat_idx}];
+            try
+                fprintf([repmat('=',1,80),'\n','Loading epoched set:', fname,'\n',repmat('=',1,80),'\n'])
+                ep_dat = pop_loadset('filename',[fname '.set'],'filepath',data_path,'verbose','off');
+                try
+                    fprintf('Checking for pac set...\n')
+                    pac_dat = pop_loadset('filename',[fname '_FZ_CZ_theta-gamma.set'],...
+                                          'filepath',pac_dir,'verbose','off');
+                    fprintf([fname ' processed. Loading next...\n'])
+                    continue
+                catch
+                    fprintf([repmat('=',1,80),'\n','Initializing PAC calclulation for ',fname,'\n',repmat('=',1,80),'\n'])
+                    fz = 'FZ'; cz = 'CZ';
+                    fz_idx = find(ismember({ep_dat.chanlocs.labels},{fz}));
+                    cz_idx = find(ismember({ep_dat.chanlocs.labels},{cz}));
+%                     ep_dat = pop_pac(ep_dat,'Channels',[4 8],[30 90],[fz_idx fz_idx cz_idx],[fz_idx cz_idx cz_idx],...
+%                                     'method','ermipac','nboot',200,'alpha',[],...
+%                                     'nfreqs1',10,'nfreqs2',20,'freqscale','log','bonfcorr',0);
+%                     ep_dat = pop_saveset(ep_dat,'filename',[fname fz '_' cz '_theta-gamma.set'],...
+%                                         'filepath',pac_dir,'savemode','onefile');
+                end
+            catch
+                fprintf([repmat('=',1,80),'\n','No trials found for ',fname,'\n',repmat('=',1,80),'\n'])
+                continue
+            end
         end
-        late = pop_loadset('filename', [num2str(subid) '_' state_conditions{sc_idx} '_late.set'],...
-                'filepath',data_path);
-        leave = pop_loadset('filename', [num2str(subid) '_' state_conditions{sc_idx} '_leave.set'],...
-                'filepath',data_path);
-%%
-        pac_dir = [data_path '/pac_data/'];
-        
-        fz = 'FZ'; cz = 'CZ';
-        fz_idx = find(ismember({early.chanlocs.labels},{fz}));
-        cz_idx = find(ismember({early.chanlocs.labels},{cz}));
-    
-        tic
-        if ~isempty(fieldnames(early))
-        early = pop_pac(early,'Channels',[4 8],[30 90],[fz_idx fz_idx cz_idx],[fz_idx cz_idx cz_idx],...
-                        'method','ermipac','nboot',200,'alpha',[],...
-                        'nfreqs1',10,'nfreqs2',20,'freqscale','log','bonfcorr',0);
-        early = pop_saveset(early,'filename',[num2str(subid) '_' state_conditions{sc_idx} '_early_' fz '_' cz '_theta-gamma.set'],...
-                            'filepath',pac_dir,'savemode','onefile');
-        end
-
-        if ~isempty(fieldnames(mid))
-        mid = pop_pac(mid,'Channels',[4 8],[30 90],[fz_idx fz_idx cz_idx],[fz_idx cz_idx cz_idx],...
-                        'method','ermipac','nboot',200,'alpha',[],...
-                        'nfreqs1',10,'nfreqs2',20,'freqscale','log','bonfcorr',0);
-        mid = pop_saveset(mid,'filename',[num2str(subid) '_' state_conditions{sc_idx} '_mid_' fz '_' cz '_theta-gamma.set'],...
-                            'filepath',pac_dir,'savemode','onefile');
-        
-        end
-
-        if ~isempty(fieldnames(late))
-        late = pop_pac(late,'Channels',[4 8],[30 90],[fz_idx fz_idx cz_idx],[fz_idx cz_idx cz_idx],...
-                        'method','ermipac','nboot',200,'alpha',[],...
-                        'nfreqs1',10,'nfreqs2',20,'freqscale','log','bonfcorr',0);
-        late = pop_saveset(late,'filename',[num2str(subid) '_' state_conditions{sc_idx} '_late_' fz '_' cz '_theta-gamma.set'],...
-                            'filepath',pac_dir,'savemode','onefile');
-        
-        end
-
-        if ~isempty(fieldnames(leave))
-        leave = pop_pac(leave,'Channels',[4 8],[30 90],[fz_idx fz_idx cz_idx],[fz_idx cz_idx cz_idx],...
-                        'method','ermipac','nboot',200,'alpha',[],...
-                        'nfreqs1',10,'nfreqs2',20,'freqscale','log','bonfcorr',0);
-        leave = pop_saveset(leave,'filename',[num2str(subid) '_' state_conditions{sc_idx} '_leave_' fz '_' cz '_theta-gamma.set'],...
-                            'filepath',pac_dir,'savemode','onefile');
-        end
-
-        toc
     end
-end    
+end
