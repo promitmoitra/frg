2153b133effd2df80c793745eb651b76e557158c
promitmoitra <promitmoitra@gmail.com>
2025-02-28 18:40:26 +0530

    frg_pacplot: WIP: modify mean_ermi to loop over stress and envs seperately | frg_pac: UPDATE: modified FZ CZ hardcode to general chan1 and chan2, running FZ and T5 TGPAC

--- frg_pac.m
+++ frg_pac.m
@@ -7,9 +7,9 @@ dir_sep = '/';
 % wrk_dir = "C:\Users\promitmoitra\Documents\GitHub\frg\";
 % dir_sep = '\';
 
-global data_path; data_path = [char(wrk_dir),'data/epoch_data/'];
+global data_path; data_path = [char(wrk_dir),'data/epoch/'];
 
-pac_dir = [data_path '/pac_data/'];
+pac_dir = [data_path '/pac/'];
 
 cd(eeglab_dir)
 eeglab nogui;
@@ -18,10 +18,11 @@ cd(data_path)
 set_fnames = dir('./*.set');
 fname_split = split({set_fnames(:).name},'_');
 subids = unique(str2num(char(fname_split(:,:,1))));
-subids = [48238];
+% subids = [15089];
 state_conditions = {'preshort','prelong', 'postshort', 'postlong'};
 trial_categories = {'early','mid','late','leave'};
-
+chan1 = 'FZ'; chan2 = 'T5';
+theta = [4 8]; gamma = [30 90];
 %%
 for idx = 1:length(subids)
     global subid; subid = subids(idx);
@@ -36,23 +37,23 @@ for idx = 1:length(subids)
                                     'filepath',data_path,'verbose','off');
                 try
                     fprintf('Checking for pac set...\n')
-                    pac_dat = pop_loadset('filename',[fname '_FZ_CZ_theta_gamma.set'],...
+                    pac_dat = pop_loadset('filename',[fname '_' chan1 '_'...
+                                          chan2 '_' 'theta_gamma.set'],...
                                           'filepath',pac_dir,'verbose','off');
                     fprintf([fname ' processed. Loading next...\n'])
                     continue
                 catch
                     fprintf([repmat('=',1,80),'\n','Initializing PAC calclulation for ',...
                             fname,'\n',repmat('=',1,80),'\n'])
-                    fz = 'FZ'; cz = 'CZ';
-                    fz_idx = find(ismember({ep_dat.chanlocs.labels},{fz}));
-                    cz_idx = find(ismember({ep_dat.chanlocs.labels},{cz}));
-                    ep_dat = pop_pac(ep_dat,'Channels',[4 8],[30 90],...
-                                    [fz_idx fz_idx cz_idx],[fz_idx cz_idx cz_idx],...
+                    chan1_idx = find(ismember({ep_dat.chanlocs.labels},{chan1}));
+                    chan2_idx = find(ismember({ep_dat.chanlocs.labels},{chan2}));
+                    ep_dat = pop_pac(ep_dat,'Channels',theta,gamma,...
+                                    repelem([chan1_idx;chan2_idx],2,1)',repmat([chan1_idx;chan2_idx],2,1)',...
                                     'method','ermipac','nboot',200,'alpha',[],...
                                     'nfreqs1',10,'nfreqs2',20,'freqscale','log','bonfcorr',0);
-                    ep_dat = pop_saveset(ep_dat,'filename',[fname ...
-                                        '_FZ_CZ_theta_gamma.set'],...
-                                        'filepath',pac_dir,'savemode','onefile');
+                    ep_dat = pop_saveset(ep_dat,'filename',[fname '_' chan1 '_'...
+                                         chan2 '_' 'theta_gamma.set'],...
+                                         'filepath',pac_dir,'savemode','onefile');
                 end
             catch
                 fprintf([repmat('=',1,80),'\n','No trials found for ',fname,'\n',...
--- frg_pacplot.m
+++ frg_pacplot.m
@@ -15,6 +15,9 @@ eeglab nogui;
 data_path = [char(wrk_dir),'data/'];
 cd(data_path)
 stai = readtable('frg_stai.csv');
+anx_score = table2array(stai(:,["STAI_Trait"]));
+trait_anx_cat = discretize(anx_score,[min(anx_score):12:max(anx_score)+12]);
+stai.("trait_anx_cat") = trait_anx_cat;
 
 pac_dir = [data_path 'epoch/pac/'];
 % pac_dir = 'E:\wrk\Neuroflow\epoch\pac';
@@ -25,18 +28,20 @@ cd(pac_dir)
 % fname_split = split({set_fnames(:).name},'_');
 % subids = unique(str2num(char(fname_split(:,:,1))));
 
-anx_cats = {'high_anx','low_anx'};
-state_conditions = {'preshort','prelong', 'postshort', 'postlong'};
+anx_cats = {'high','mid','low'};
+stress_conditions = {'pre','post'};
+tt_envs = {'short','long'};
 trial_categories = {'early','mid','late','leave'};
 chan1 = 'FZ'; chan2 = 'CZ'; band1 = 'theta'; band2 = 'gamma';
+
 
 badids = [37532 38058 39862 43543 45528 47801 48278];
 % subids = [31730,43000];
 stai = sortrows(stai,"STAI_Trait");
-high_anx_subids = stai{end-16:end,["ParticipantID"]};
+high_anx_subids = stai{end-(num_sub+1):end,["ParticipantID"]};
 high_anx_subids = setdiff(high_anx_subids,badids)
 
-low_anx_subids = stai{1:15,["ParticipantID"]};
+low_anx_subids = stai{1:num_sub,["ParticipantID"]};
 low_anx_subids = setdiff(low_anx_subids,badids)
 
 %%
@@ -66,7 +71,7 @@ low_anx_subids = setdiff(low_anx_subids,badids)
 % [outdata,erp] = frg_plottrialpac(eeg_dat,'plotindx',chan_pair_idx,...
 %                             'timerange',[-500 0],'freqval1',5.5,'freqval2',30);
 
-%%
+%% WIP: modify mean_ermi to loop over stress and envs seperately
 mean_ermi = struct();
 
 for sc_idx = 1:length(state_conditions)
@@ -121,8 +126,8 @@ mean_ermi=load('./ermi.mat');
 %%
 chan_pair = 'FZ_CZ';
 
-% anx_cat = 'low_anx';
-anx_cat = 'high_anx';
+anx_cat = 'low_anx';
+% anx_cat = 'high_anx';
 
 % sc = 'preshort';
 % sc = 'prelong';
@@ -131,17 +136,18 @@ sc = 'postshort';
 
 % x = mean_ermi.(sc).early.high_anx.tpts;
 
-% txt = 'short'; var_lvl = sc;
-txt = 'pre'; var_lvl = sc;
+txt = 'short'; var_lvl = sc;
+% txt = 'pre'; var_lvl = sc;
 % txt = 'low'; var_lvl = anx_cat;
+
 if contains(var_lvl,txt)
     figure('OuterPosition',[246 131 922 923]);hold on;
-%     title('Effect of anxiety and travel time - post stress')
+%     title('Effect of anxiety and travel time - pre stress')
 %     labels = {'low anx short','high anx short','low anx long','high anx long'};
 %     title('Effect of anxiety and stress - long')
 %     labels = {'low anx pre','high anx pre','low anx post','high anx post'};
-    title('Effect of stress and travel time - high anx')
-    labels = {'pre short','pre long','post short','post long'};
+%     title('Effect of stress and travel time - high anx')
+%     labels = {'pre short','pre long','post short','post long'};
 end    
 ax=gca;
 lwid = 2; msiz = 8;
@@ -167,12 +173,15 @@ end
 % legend;
 
 % anx_cat = 'low_anx';
-% anx_cat = 'high_anx';
+anx_cat = 'high_anx';
 
 % sc = 'preshort';
-% sc = 'prelong';
+sc = 'prelong';
 % sc = 'postshort';
-sc = 'postlong';
+% sc = 'postlong';
+
+% var_lvl = sc;
+% var_lvl = anx_cat;
 
 for tc = 1:length(trial_categories)
     tcat = trial_categories{tc};
@@ -190,7 +199,7 @@ for tc = 1:length(trial_categories)
     end
     xticks(1:4);xticklabels(trial_categories);xlim([0.5 4.5])
 end
-xlabel('Patch level latency');ylabel('FZ-CZ Theta-Gamma Event Rel. PAC Mod. Index');
+xlabel('Patch level latency');ylabel('FZ-CZ Theta-Gamma ERMIPAC Modulation Index');
 
 if ~contains(var_lvl,txt)
     hleg = legend([hbo,hro,hbx,hrx],labels);
