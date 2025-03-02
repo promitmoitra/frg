7828d186556a5d999e81680144f1e1513b73031b
promitmoitra <promitmoitra@gmail.com>
2025-03-02 15:44:54 +0530

    final plotting for FZ_CZ_TGPAC

--- frg_pacplot.m
+++ frg_pacplot.m
@@ -4,23 +4,23 @@ clear;clc;
 % wrk_dir = '/home/decision_lab/work/github/frg/';
 % dir_sep = '/';
 
-% dir_sep = '\';
+dir_sep = '\';
 % eeglab_dir = "C:\Users\promitmoitra\Documents\MATLAB\eeglab2023.1\";
-% eeglab_dir='E:\wrk\MATLAB Add-Ons\Collections\EEGLAB';
-% wrk_dir = "C:\Users\promitmoitra\Documents\GitHub\frg\";
+eeglab_dir='E:\wrk\MATLAB Add-Ons\Collections\EEGLAB';
+wrk_dir = "C:\Users\promitmoitra\Documents\GitHub\frg\";
 
-cd(eeglab_dir)
-eeglab nogui;
+% cd(eeglab_dir)
+% eeglab nogui;
 
 data_path = [char(wrk_dir),'data/'];
-cd(data_path)
+cd(wrk_dir)
 stai = readtable('frg_stai.csv');
 anx_score = table2array(stai(:,["STAI_Trait"]));
 trait_anx_cat = discretize(anx_score,[min(anx_score):12:max(anx_score)+12]);
 stai.("trait_anx_cat") = trait_anx_cat;
 
-pac_dir = [data_path 'epoch/pac/'];
-% pac_dir = 'E:\wrk\Neuroflow\epoch\pac';
+% pac_dir = [data_path 'epoch/pac/'];
+pac_dir = 'E:\wrk\Neuroflow\epoch\pac';
 cd(pac_dir)
 
 %%
@@ -29,7 +29,7 @@ cd(pac_dir)
 % subids = unique(str2num(char(fname_split(:,:,1))));
 
 anx_cats = {'low','mid','high'};
-stress_conditions = {'pre','post'};
+stress_conds = {'pre','post'};
 tt_envs = {'short','long'};
 trial_categories = {'early','mid','late','leave'};
 chan1 = 'FZ'; chan2 = 'CZ'; band1 = 'theta'; band2 = 'gamma';
@@ -44,9 +44,8 @@ badids = [37532 38058 39862 43543 45528 47801 48278];
 % 
 % low_anx_subids = stai{1:num_sub,["ParticipantID"]};
 % low_anx_subids = setdiff(low_anx_subids,badids)
-%%
+%
 % sub_idx = 1; global subid; subid = subids(sub_idx);
-% 
 % sc_idx = 1;
 % cat_idx = 1;
 % % chan_pair = [chan1 '-' chan1];
@@ -76,10 +75,11 @@ badids = [37532 38058 39862 43543 45528 47801 48278];
 % for anx_cat_idx = 1:length(anx_cats)
 %     anx_cat = anx_cats{anx_cat_idx};
 %     subids = table2array(stai(stai.trait_anx_cat==anx_cat_idx,["ParticipantID"]));
+%     subids = setdiff(subids,badids);
 %     mean_ermi.(anx_cat) = struct();
-% 
-%     for sc_idx = 1:length(stress_conditions)
-%         sc = stress_conditions{sc_idx};
+%     mean_ermi.(anx_cat).tpts = [];
+%     for sc_idx = 1:length(stress_conds)
+%         sc = stress_conds{sc_idx};
 %         mean_ermi.(anx_cat).(sc) = struct();
 %         for env_idx = 1:length(tt_envs)
 %             env = tt_envs{env_idx};
@@ -88,7 +88,7 @@ badids = [37532 38058 39862 43543 45528 47801 48278];
 %             for cat_idx = 1:length(trial_categories)
 %                 tcat = trial_categories{cat_idx};
 %                 mean_ermi.(anx_cat).(sc).(env).(tcat) = struct();
-%     
+% 
 %                 for sub_idx = 1:length(subids)
 %                     subid = subids(sub_idx);
 %                     fname = strjoin({num2str(subid),sctt,tcat,chan1,chan2,band1,band2},'_');
@@ -113,171 +113,169 @@ badids = [37532 38058 39862 43543 45528 47801 48278];
 %                         % pause(3)
 %                     end
 %                 end
-%                 mean_ermi.(anx_cat).(sc).(env).(tcat).tpts = tpts;
 %             end
 %         end
 %     end
+%     mean_ermi.(anx_cat).tpts = tpts;
 % end
+% cd(wrk_dir)
 % save('ermi.mat','mean_ermi')
 %%
 % fig_upper = figure('OuterPosition',[-0.0158    0.3962    1.5536    0.4688]*1e3); hold on;
 cd(wrk_dir);
-mean_ermi=load('./ermi.mat');
+% mean_ermi=
+load('./ermi.mat');
 %%
 chan_pair = 'FZ_CZ';
 
-anx_cat = 'low_anx';
-% anx_cat = 'high_anx';
+anx_cats = {'low','mid'};
+% anx_cats = {'mid','high'};
+% anx_cats = {'low','high'};
+stress_conds = {'pre','post'};
+tt_envs = {'short','long'};
 
-% sc = 'preshort';
-% sc = 'prelong';
-sc = 'postshort';
-% sc = 'postlong';
+% var_lvl1=anx_cats;
+% var_lvl2=stress_conds;
+% tt = 'short';
+% % tt = 'long';
+% var_fixed=tt;
+% var_order=[1,2,3];
 
-% x = mean_ermi.(sc).early.high_anx.tpts;
+% var_lvl1=stress_conds;
+% var_lvl2=tt_envs;
+% anx_cat = 'low';
+% % anx_cat = 'mid';
+% % anx_cat = 'high';
+% var_fixed=anx_cat;
+% var_order=[3,1,2];
 
-txt = 'short'; var_lvl = sc;
-% txt = 'pre'; var_lvl = sc;
-% txt = 'low'; var_lvl = anx_cat;
+% var_lvl1=tt_envs;
+% var_lvl2=anx_cats;
+% sc = 'pre';
+% % sc = 'post';
+% var_fixed=sc;
+% var_order=[2,3,1];
 
-if contains(var_lvl,txt)
-    figure('OuterPosition',[246 131 922 923]);hold on;
-%     title('Effect of anxiety and travel time - pre stress')
-%     labels = {'low anx short','high anx short','low anx long','high anx long'};
-%     title('Effect of anxiety and stress - long')
-%     labels = {'low anx pre','high anx pre','low anx post','high anx post'};
-%     title('Effect of stress and travel time - high anx')
-%     labels = {'pre short','pre long','post short','post long'};
-end    
-ax=gca;
-lwid = 2; msiz = 8;
+figure('OuterPosition',[246 131 922 923]);hold on;
+hdl=[];lbl={};
+clrs=['b','r'];mrks=['o','s'];
 
-% t_mean = [];
-% sub_mean = [];
-for tc = 1:length(trial_categories)
-    tcat = trial_categories{tc};
-    y = mean_ermi.(sc).(tcat).(anx_cat).(chan_pair).ermi;
-%     y_sub_mean = mean(y,2);
-%     y_err = std(y_sub_mean)/sqrt(length(y_sub_mean));
-%     sub_mean(:,end+1) = y_sub_mean;
-    y_t_mean = mean(y,1);
-    y_t_sub_mean = mean(y,'all');
-    y_err = std(y_t_mean)/sqrt(length(y_t_mean));
-    if contains(var_lvl,txt)
-        hbo=errorbar(tc,y_t_sub_mean,y_err,'bo','LineWidth',lwid,'MarkerSize',msiz);%,'DisplayName',[tcat '- low'])
-    else
-        hbx=errorbar(tc,y_t_sub_mean,y_err,'bx','LineWidth',lwid,'MarkerSize',msiz);
+for var_idx1 = 1:length(var_lvl1)
+    var_name1 = var_lvl1{var_idx1};
+    for var_idx2 = 1:length(var_lvl2)
+        var_name2 = var_lvl2{var_idx2};
+        var_n = {var_name1,var_name2,var_fixed};
+        lbl{end+1} = strjoin(var_n(var_order),'-');
+        for tc = 1:length(trial_categories)
+            tcat = trial_categories{tc};
+            %hardcoded
+            y = eval(['mean_ermi.' strjoin(var_n(var_order),'.') '.(tcat).(chan_pair).ermi']);
+            y_t_mean = mean(y,1);
+            y_t_sub_mean = mean(y,'all');
+            y_err = std(y_t_mean)/sqrt(length(y_t_mean));
+            clr=clrs(var_idx1);mrk=mrks(var_idx2);
+            h = errorbar(tc,y_t_sub_mean,y_err,[clr mrk],'LineWidth',lwid,'MarkerSize',msiz);
+            xticks(1:4);xticklabels(trial_categories);xlim([0.5 4.5])
+        end
+        hdl(end+1) = h;
     end
-    xticks(1:4);xticklabels(trial_categories);xlim([0.5 4.5])
 end
-% legend;
+legend(hdl,lbl)
+%%
+anx_cats = {'low','mid','high'};
+% anx_cats = {'low','high'};
+stress_conds = {'pre','post'};
+tt_envs = {'short','long'};
 
-% anx_cat = 'low_anx';
-anx_cat = 'high_anx';
+% var_lvl1=anx_cats;
+% sc = 'pre';
+% % sc = 'post';
+% var_fixed1=sc;
+% tt = 'short';
+% % tt = 'long';
+% var_fixed2=tt;
+% var_order=[1,2,3];
 
-% sc = 'preshort';
-sc = 'prelong';
-% sc = 'postshort';
-% sc = 'postlong';
+% var_lvl1=stress_conds;
+% tt = 'short';
+% % tt = 'long';
+% var_fixed1=tt;
+% anx_cat = 'low';
+% % anx_cat = 'mid';
+% % anx_cat = 'high';
+% var_fixed2=anx_cat;
+% var_order=[3,1,2];
 
-% var_lvl = sc;
-% var_lvl = anx_cat;
+var_lvl1=tt_envs;
+anx_cat = 'low';
+% anx_cat = 'mid';
+% anx_cat = 'high';
+var_fixed1=anx_cat;
+sc = 'pre';
+% sc = 'post';
+var_fixed2=sc;
+var_order=[2,3,1];
 
-for tc = 1:length(trial_categories)
-    tcat = trial_categories{tc};
-    y = mean_ermi.(sc).(tcat).(anx_cat).(chan_pair).ermi;
-%     y_sub_mean = mean(y,2);
-%     y_err = std(y_sub_mean)/sqrt(length(y_sub_mean));
-%     sub_mean(:,end+1) = y_sub_mean;
-    y_t_mean = mean(y,1);
-    y_t_sub_mean = mean(y,'all');
-    y_err = std(y_t_mean)/sqrt(length(y_t_mean));
-    if contains(var_lvl,txt)
-        hro=errorbar(tc,y_t_sub_mean,y_err,'ro','LineWidth',lwid,'MarkerSize',msiz);%,'DisplayName',[tcat '- high'])
-    else
-        hrx=errorbar(tc,y_t_sub_mean,y_err,'rx','LineWidth',lwid,'MarkerSize',msiz);
+figure('OuterPosition',[246 131 922 923]);hold on;
+hdl=[];lbl={};
+clrs=['b','g','r'];mrks=['o','s','d'];
+
+for var_idx1 = 1:length(var_lvl1)
+    var_name1 = var_lvl1{var_idx1};
+    var_n = {var_name1,var_fixed1,var_fixed2};
+    lbl{end+1} = strjoin(var_n(var_order),'-');
+    for tc = 1:length(trial_categories)
+        tcat = trial_categories{tc};
+        y = eval(['mean_ermi.' strjoin(var_n(var_order),'.') '.(tcat).(chan_pair).ermi']);
+        y_t_mean = mean(y,1);
+        y_t_sub_mean = mean(y,'all');
+        y_err = std(y_t_mean)/sqrt(length(y_t_mean));
+        clr=clrs(var_idx1);mrk=mrks(var_idx1);
+        h = errorbar(tc,y_t_sub_mean,y_err,[clr mrk],'LineWidth',lwid,'MarkerSize',msiz);
+        xticks(1:4);xticklabels(trial_categories);xlim([0.5 4.5])
     end
-    xticks(1:4);xticklabels(trial_categories);xlim([0.5 4.5])
+    hdl(end+1) = h;
 end
-xlabel('Patch level latency');ylabel('FZ-CZ Theta-Gamma ERMIPAC Modulation Index');
+legend(hdl,lbl)
+%%
+num_plot=2;
+% anx_cat='low';sc='post';tt='long';
+% var_n{1} = {anx_cat,sc,tt};
+% anx_cat='mid';sc='pre';tt='long';
+% var_n{2} = {anx_cat,sc,tt};
 
-if ~contains(var_lvl,txt)
-    hleg = legend([hbo,hro,hbx,hrx],labels);
-end    
+anx_cat='low';sc='pre';tt='long';
+var_n{1} = {anx_cat,sc,tt};
+anx_cat='mid';sc='post';tt='short';
+var_n{2} = {anx_cat,sc,tt};
 
+hdl=[];lbl={};
+clrs = ['b','r'];
+for idx = 1:num_plot
+    if idx==1
+        figure('OuterPosition',[246 131 922 923]);hold on;
+    end
+    lbl{end+1} = strjoin(var_n{idx},'-');
+    for tc = 1:length(trial_categories)
+        tcat = trial_categories{tc};
+        y = eval(['mean_ermi.' strjoin(var_n{idx},'.') '.(tcat).(chan_pair).ermi']);
+        y_t_mean = mean(y,1);
+        y_t_sub_mean = mean(y,'all');
+        y_err = std(y_t_mean)/sqrt(length(y_t_mean));
+        clr=clrs(idx);mrk='o';
+        h = errorbar(tc,y_t_sub_mean,y_err,[clr mrk],'LineWidth',lwid,'MarkerSize',msiz);
+        xticks(1:4);xticklabels(trial_categories);xlim([0.5 4.5])
+    end
+    hdl(end+1) = h;
+end
+legend(hdl,lbl)
+%%
 %%% Discuss what to plot: fluctuation around temporal mean (y_sub_err) 
 %%% and across subject variability at each timepoint (y_t_err)
-%%% errorbar(1:4,[y1_mean,y2_mean,y3_mean,y4_mean],[y1_sem,y2_sem,y3_sem,y4_sem],'DisplayName',[anx_cat,' ',sc])
-%%
-% y1 = mean(mean_ermi.(sc).early.(anx_cat).(chan_pair).ermi,2);
-% y2 = mean(mean_ermi.(sc).mid.(anx_cat).(chan_pair).ermi  ,2);
-% y3 = mean(mean_ermi.(sc).late.(anx_cat).(chan_pair).ermi ,2);
-% y4 = mean(mean_ermi.(sc).leave.(anx_cat).(chan_pair).ermi,2);
-% % plot(x,y1,'DisplayName','early');
-% % plot(x,y2,'DisplayName','mid');
-% % plot(x,y3,'DisplayName','late');
-% % plot(x,y4,'DisplayName','leave');
-% 
-% y1_mean = mean(mean_ermi.(sc).early.(anx_cat).(chan_pair).ermi,'all');
-% y2_mean = mean(mean_ermi.(sc).mid.(anx_cat).(chan_pair).ermi  ,'all');
-% y3_mean = mean(mean_ermi.(sc).late.(anx_cat).(chan_pair).ermi ,'all');
-% y4_mean = mean(mean_ermi.(sc).leave.(anx_cat).(chan_pair).ermi,'all');
-% % plot(x,y1_mean*ones(size(x)),'DisplayName','early');
-% % plot(x,y2_mean*ones(size(x)),'DisplayName','mid');
-% % plot(x,y3_mean*ones(size(x)),'DisplayName','late');
-% % plot(x,y4_mean*ones(size(x)),'DisplayName','leave');
-% 
-% y1_sem = std(y1)/sqrt(length(y1));
-% y2_sem = std(y2)/sqrt(length(y2));
-% y3_sem = std(y3)/sqrt(length(y3));
-% y4_sem = std(y4)/sqrt(length(y4));
-% % errorbar(x,y1,y1_sem,'--k','HandleVisibility','off');
-% % errorbar(x,y2,y2_sem,'--k','HandleVisibility','off');
-% % errorbar(x,y3,y3_sem,'--k','HandleVisibility','off');
-% % errorbar(x,y4,y4_sem,'--k','HandleVisibility','off');
-% % legend;
-% 
-% anx_cat(strfind(anx_cat,'_'))='-';
-% errorbar(1:4,[y1_mean,y2_mean,y3_mean,y4_mean],[y1_sem,y2_sem,y3_sem,y4_sem],'DisplayName',[anx_cat,' ',sc])
-% 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% anx_cat = 'high_anx';
-% % anx_cat = 'low_anx';
-% 
-% % fig_lower = figure('OuterPosition',[-0.0078    0.0362    1.5504    0.4480]*1e3); hold on;
-% 
-% % sc = 'preshort'; 
-% % sc = 'prelong'; 
-% % sc = 'postshort';
-% % sc = 'postlong'; 
-% 
-% y1 = mean(mean_ermi.(sc).early.(anx_cat).(chan_pair).ermi,2);
-% y2 = mean(mean_ermi.(sc).mid.(anx_cat).(chan_pair).ermi  ,2);
-% y3 = mean(mean_ermi.(sc).late.(anx_cat).(chan_pair).ermi ,2);
-% y4 = mean(mean_ermi.(sc).leave.(anx_cat).(chan_pair).ermi,2);
-% % plot(x,y1,'DisplayName','early');
-% % plot(x,y2,'DisplayName','mid');
-% % plot(x,y3,'DisplayName','late');
-% % plot(x,y4,'DisplayName','leave')
-% 
-% y1_mean = mean(mean_ermi.(sc).early.(anx_cat).(chan_pair).ermi,'all');
-% y2_mean = mean(mean_ermi.(sc).mid.(anx_cat).(chan_pair).ermi  ,'all');
-% y3_mean = mean(mean_ermi.(sc).late.(anx_cat).(chan_pair).ermi ,'all');
-% y4_mean = mean(mean_ermi.(sc).leave.(anx_cat).(chan_pair).ermi,'all');
-% % plot(x,y1_mean*ones(size(x)),'DisplayName','early');
-% % plot(x,y2_mean*ones(size(x)),'DisplayName','mid');
-% % plot(x,y3_mean*ones(size(x)),'DisplayName','late');
-% % plot(x,y4_mean*ones(size(x)),'DisplayName','leave');
-% 
-% y1_sem = std(y1)/sqrt(length(y1));
-% y2_sem = std(y2)/sqrt(length(y2));
-% y3_sem = std(y3)/sqrt(length(y3));
-% y4_sem = std(y4)/sqrt(length(y4));
-% % errorbar(x,y1,y1_sem,'--k','HandleVisibility','off');
-% % errorbar(x,y2,y2_sem,'--k','HandleVisibility','off');
-% % errorbar(x,y3,y3_sem,'--k','HandleVisibility','off');
-% % errorbar(x,y4,y4_sem,'--k','HandleVisibility','off');
-% 
-% anx_cat(strfind(anx_cat,'_'))='-';
-% errorbar(1:4,[y1_mean,y2_mean,y3_mean,y4_mean],[y1_sem,y2_sem,y3_sem,y4_sem],'DisplayName',[anx_cat,' ',sc])
-% legend;
+% %     title('Effect of anxiety and travel time - pre/post stress')
+% %     labels = {'low anx short','high anx short','low anx long','high anx long'};
+% %     title('Effect of anxiety and stress - short/long')
+% %     labels = {'low anx pre','high anx pre','low anx post','high anx post'};
+% %     title('Effect of stress and travel time - low/high')
+% %     labels = {'pre short','pre long','post short','post long'};
+% end    
